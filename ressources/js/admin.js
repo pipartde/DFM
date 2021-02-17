7d13c12f24e87dc0d1a5ac5bd3206bc0289b7fc6
pipartde <denis.pipart@gmail.com>
2021-02-17 11:31:12 +0100

    update : hide des éléments non accessible au non superadmin ok update : transformation du login en logout si connecté ou non update : rapide amélio du front pour le login et pagadmin

---
+++ ressources/js/admin.js
@@ -0,0 +1,9 @@
+//-------------------------------------------------------//
+// affichage selon accès autorisé
+//-------------------------------------------------------//
+function testing() {
+    $(document).find(".superadmin").each(function () {
+        $(this).addClass("hide")
+    })
+}
+
